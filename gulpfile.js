862dc262b5971eab940cceac5165be19efc2f6a2
lachena <lachen@monkeyman.ru>
2019-12-15 04:49:06 +0200

    Добавляет корректировку gulpfile.js для оптимизации изображений

--- gulpfile.js
+++ gulpfile.js
@@ -28,11 +28,11 @@ gulp.task("css", function () {
 
 gulp.task("images", function () {
   return gulp.src("source/img/**/*.{png,jpg,svg}")
-  pipe(imagemin)[
+  .pipe(imagemin([
     imagemin.optipng({optimizationLevel: 3}),
     imagemin.jpegtran({progressive: true}),
     imagemin.svgo()
-  ]
+  ]))
   .pipe(gulp.dest("source/img"));
 });
 
