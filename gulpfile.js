d70909f1e4dfe4e8df7b65175fc377321ce563e1
lachena <lachen@monkeyman.ru>
2019-12-15 04:56:33 +0200

    Добавляет оптимизацию изображений в формат webp

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var csso = require("gulp-csso");
 var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
 var server = require("browser-sync").create();
 
 gulp.task("css", function () {
@@ -28,13 +29,19 @@ gulp.task("css", function () {
 
 gulp.task("images", function () {
   return gulp.src("source/img/**/*.{png,jpg,svg}")
-  .pipe(imagemin([
-    imagemin.optipng({optimizationLevel: 3}),
-    imagemin.jpegtran({progressive: true}),
-    imagemin.svgo()
-  ]))
-  .pipe(gulp.dest("source/img"));
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
 });
+
+gulp.task("webp", function (){
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 85}))
+    .pipe(gulp.dest("source/img"));
+})
 
 gulp.task("server", function () {
   server.init({
