e5f983b10cdb7cc73c1717b6bcd727101ae3f726
lachena <lachen@monkeyman.ru>
2020-01-08 14:00:19 +0200

    Добавляет карту с помощью js

---
+++ source/js/map.js
@@ -0,0 +1,13 @@
+ymaps.ready(init);
+  function init(){
+      var myMap = new ymaps.Map("map", {
+          center: [59.938631, 30.323055],
+          zoom: 17
+      });
+      pointer = new ymaps.Placemark([59.938631, 30.323055], {
+      iconLayout: "default#image",
+      iconImageHref: "./img/map-pin.png",
+      iconImageSize: [19, 19]
+      });
+      myMap.geoObjects.add(pointer);
+  }
