4987b65f505b1065cccf6c5c108cf8e3a61d2aea
lachena <lachen@monkeyman.ru>
2020-01-19 09:36:59 +0200

    Добавляет корректные кавычки в js-файлах

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,14 +1,14 @@
-var header = document.querySelector('.header');
-var toggle = document.querySelector('.header__toggle');
+var header = document.querySelector(".header");
+var toggle = document.querySelector(".header__toggle");
 
-header.classList.remove('header--nojs');
+header.classList.remove("header--nojs");
 
-  toggle.addEventListener('click', function() {
-    if (header.classList.contains('header--closed')) {
-      header.classList.remove('header--closed');
-      header.classList.add('header--opened');
+  toggle.addEventListener("click", function() {
+    if (header.classList.contains("header--closed")) {
+      header.classList.remove("header--closed");
+      header.classList.add("header--opened");
     } else {
-      header.classList.add('header--closed');
-      header.classList.remove('header--opened');
+      header.classList.add("header--closed");
+      header.classList.remove("header--opened");
     }
   });
